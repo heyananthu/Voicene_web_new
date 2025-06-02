a3b0299be271feb80f0b89f518ebd53f6e0b4d74
ananthu <heyananthulal@gmail.com>
2025-06-02 07:44:43 +0530

    first commit

---
+++ Js/corporateprofile.js
@@ -0,0 +1,3 @@
+ // Duplicate content dynamically for seamless infinite scrolling
+ const scrollContainer = document.getElementById("scroll-container");
+ scrollContainer.innerHTML += scrollContainer.innerHTML; // Duplicate content
