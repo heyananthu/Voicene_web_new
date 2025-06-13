d0a45edf323a39dc2adfc3d3736ccc6512c30bf5
ananthu <heyananthulal@gmail.com>
2025-06-13 14:24:10 +0530

    casestudy added

---
+++ casestudy/animationScript.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const card = document.getElementById('animatedCard');
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.2
+    };
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                card.classList.add('animate-fade-up');
+            } else {
+                card.classList.remove('animate-fade-up');
+            }
+        });
+    }, observerOptions);
+    observer.observe(card);
+});
